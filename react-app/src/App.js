1d57553d45f2cb88bb6e0ef28cb3e5b7d14a394b
Rajiv <quadruplestak@gmail.com>
2021-04-06 06:14:38 -0400

    partially finished Landing Page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authenticate } from "./store/session";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
-import { authenticate } from "./services/auth";
+// import ProtectedRoute from "./components/auth/ProtectedRoute";
+import LandingPage from "./components/LandingPage";
 
 function App() {
+  const dispatch = useDispatch();
   const [authenticated, setAuthenticated] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    (async() => {
-      const user = await authenticate();
+    const func = async() => {
+      const user = await dispatch(authenticate());
       if (!user.errors) {
         setAuthenticated(true);
       }
       setLoaded(true);
-    })();
-  }, []);
+    };
+    func();
+  }, [dispatch]);
 
   if (!loaded) {
     return null;
@@ -28,7 +30,9 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
+      <NavBar
+        authenticated={authenticated} 
+        setAuthenticated={setAuthenticated} />
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm
@@ -39,7 +43,10 @@ function App() {
         <Route path="/sign-up" exact={true}>
           <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
         </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
+        <Route path="/" exact={true} authenticated={authenticated}>
+          <LandingPage authenticated={authenticated} />
+        </Route>
+        {/* <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
@@ -47,7 +54,7 @@ function App() {
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
           <h1>My Home Page</h1>
-        </ProtectedRoute>
+        </ProtectedRoute> */}
       </Switch>
     </BrowserRouter>
   );
